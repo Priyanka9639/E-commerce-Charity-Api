8160a8183be8da81940b0a88f6c0d9a6040fa2c5
Priyanka <priyankabln963@gmail.com>
2025-07-30 17:59:28 +0530

    Config to  ConnecttoMongoDB

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const express = require("express")
+const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express()
 
 require("dotenv").config();
 const PORT  = process.env.PORT||8080
+const url = process.env.MongoDB_URL
+
+function connectMongoDB(){
+    try{
+
+    mongoose.connect(url);
+    console.log(" connected to mongoDB");
+    }catch(err){
+        console.log("Error to connect mongoDB");
+    }
+   
+}
 
 
-
-app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
 
 app.listen(8080,()=>{
+    connectMongoDB()
 console.log("Server port number http://localhost:8080");
 })
